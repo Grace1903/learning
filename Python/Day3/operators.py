62dbb5fdee758f49202962f2e3d949ab281e1ebf
Grace Elizabeth Jose <129204450+Grace1903@users.noreply.github.com>
2026-05-17 18:31:45 +0530

    Completed Python Day 4 strings

---
+++ Python/Day3/operators.py
@@ -0,0 +1,38 @@
+# Arithmetic operators
+
+a = 10
+b = 3
+
+print(a + b)   # addition
+print(a - b)   # subtraction
+print(a * b)   # multiplication
+print(a / b)   # division#3.33333333
+print(a % b)   # modulus#1
+print(a ** b)  # power
+print(a // b)  # floor division#3
+
+# Assignment operators
+x = 5
+
+x += 2
+print(x)
+
+x -= 1
+print(x)
+
+x *= 2
+print(x)
+
+# Comparison operators
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+
+# Logical operators
+print(True and True)
+print(True and False)
+
+print(True or False)
+
+print(not True)
