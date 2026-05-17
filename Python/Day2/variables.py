1981dc980317bf8d6cc376e6cb4c45d3ae60471d
Grace Elizabeth Jose <129204450+Grace1903@users.noreply.github.com>
2026-05-17 18:07:20 +0530

    Updated Day 2 practice

--- Python/Day2/variables.py
+++ Python/Day2/variables.py
@@ -1,5 +1,4 @@
 # Day 2 - Variables and Built-in Functions
-'''
 first_name = "Grace"
 last_name = "Elizabeth"
 country = "India"
@@ -18,13 +17,13 @@ age = int(input("Enter your age: "))
 
 print(age)
 print(type(age))
-
+'''
 Enter your age: 22.5
 asked for int gave float input
     age = int(input("Enter your age: "))
           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 ValueError: invalid literal for int() with base 10: '22.5'
-
+'''
 first_name, last_name, country = "Grace", "Elizabeth", "India"
 
 print(first_name)
@@ -34,7 +33,6 @@ age = 21
 
 print(str(age))
 print(type(str(age)))
-'''
 num = int(5.76575849494040)
 
 print(num)
@@ -56,4 +54,5 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 print(num1 + num2)
-print(Name)#NameError ...u should hv typed name not Name
+
+#print(Name)#NameError ...u should hv typed name not Name
